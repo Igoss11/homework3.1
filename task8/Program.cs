e822fdfe7812615a58a4d1f1330b604fb7856825
Igor <igoss11@gmail.com>
2022-09-19 16:46:21 +0300

    finish task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int a = 1;
+bool not = true;
+Console.WriteLine("Четные числа от 1 до " + number + ":");
+while (a <= number)
+{
+    if (a % 2 != 1)
+        {
+            Console.Write(a + "   ");
+            not = false;
+        }
+    a++;
+}
+if (not)
+    {
+        Console.WriteLine("Нет четных чисел");
+    }
