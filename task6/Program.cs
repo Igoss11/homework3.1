75994485569123c9c726faf56cb4bb71c47fe557
Igor <igoss11@gmail.com>
2022-09-19 16:18:12 +0300

    finish task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
