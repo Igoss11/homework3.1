b761436d8f70f9fffb37ef5052735bc6016dd8c9
Igor <igoss11@gmail.com>
2022-09-19 15:29:52 +0300

    finish task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Число a - большее");
+    Console.WriteLine("Число b - меньшее");
+}
+else if(a < b)
+{
+    Console.WriteLine("Число b - большее");
+    Console.WriteLine("Число a - меньшее");
+}
