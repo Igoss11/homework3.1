5b27da601de884687aaa71014d60abc13f739e7a
Igor <igoss11@gmail.com>
2022-09-19 15:50:47 +0300

    finish task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = int.Parse(Console.ReadLine());
+if(a > b)
+{
+    if(a > c)
+    {
+        Console.WriteLine("Число a - max");
+    }
+    else if(a < c)
+    {
+        Console.WriteLine("Число c - max");
+    }
+}
+else
+{
+    if(b > c)
+    {
+        Console.WriteLine("Число b - max");
+    }
+    else if(b < c)
+    {
+        Console.WriteLine("Число c - max");
+    }
+}
